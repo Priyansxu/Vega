95c4f7a97d73172e036d9f7899ab807c27ed5396
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2026-01-19 11:19:09 +0530

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,12 +1,20 @@
 export const dynamic = 'force-static'
 
 export default function sitemap() {
+  const lastModified = new Date()
+
   return [
     {
       url: 'https://vega.js.org',
-      lastModified: new Date(),
+      lastModified,
       changeFrequency: 'monthly',
       priority: 1,
+    },
+    {
+      url: 'https://vega.js.org/create',
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
   ]
 }
